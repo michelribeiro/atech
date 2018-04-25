b370e29557e1bd1c703edfc0e0eeb44183b19fec
Michel Ribeiro <michelribeiro2004@gmail.com>
2018-04-25 17:52:19 -0300

    toogle menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,11 @@ class Header extends Component {
         super(props);
 
         this.state = {
-            isTop: true
+            isTop: true,
+            ativaMenu: false
         }
         this.renderScroll = this.renderScroll.bind(this);
+        this.showMenu = this.showMenu.bind(this);
     }
     
     componentDidMount() {
@@ -29,6 +31,11 @@ class Header extends Component {
     componentWillUnmount() {
         document.removeEventListener('scroll', this.renderScroll)
     }
+    showMenu() {
+        this.setState(prevState => ({
+            ativaMenu: !prevState.ativaMenu
+        }))
+    }
     
     render() {
 
@@ -38,33 +45,17 @@ class Header extends Component {
 
                     <div className="content">
                         <div className="sub_media">
-                            <ul className="small right">
-                                <li><a href="#">Apps</a></li>
-                                <li><a href="#">Fóruns</a></li>
-                                <li><a href="#">Placar de colaboradores</a></li>
-                                <li><a href="#">Contribua</a></li>
-                                <li><a href="#">API</a></li>
-                                <li><a href="#">Suporte</a></li>
-                            </ul>
-                            <div className="left">
+
+                            <div className="onlyMobile">
+                                <p onClick={this.showMenu.bind(this, true)}>
+                                    <Glyphicon glyph="menu-hamburger" />
+                                </p>
+
+                                <a href="/">
+                                    <img src="https://www.themoviedb.org/static_cache/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" title="The MovieDB" alt="The MovieDB" width="91" height="81" />
+                                </a>
+
                                 <ul className="primary">
-                                    <li className="logo">
-                                        <a href="/">
-                                            <img src="https://www.themoviedb.org/static_cache/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" title="The MovieDB" alt="The MovieDB" width="91" height="81" />
-                                            <span className="title">The Movie DB</span>
-                                            {/* <Glyphicon glyph="asterisk" /> */}
-                                        </a>
-                                    </li>
-                                    <li><a href="#">Explorar</a></li>
-                                    <li><a href="#">Filmes</a></li>
-                                    <li><a href="#">Séries</a></li>
-                                    <li><a href="#">Pessoas</a></li>
-                                </ul>
-                            </div>
-                            <div className="right">
-                                <ul className="primary">
-                                    <li><a href="#"><Glyphicon glyph="plus" /></a></li>
-                                    <li><a href="#"><Glyphicon glyph="globe" /></a></li>
                                     <li><a href="#"><Glyphicon glyph="bell" /></a></li>
                                     <li className="user">
                                         <a href="#">
@@ -74,6 +65,47 @@ class Header extends Component {
                                         </a>
                                     </li>
                                 </ul>
+                            </div>
+
+                            <div className={this.state.ativaMenu ? "navMobile active" : "navMobile"} >
+
+                                <ul className="small right">
+                                    <li><a href="#">Apps</a></li>
+                                    <li><a href="#">Fóruns</a></li>
+                                    <li><a href="#">Placar de colaboradores</a></li>
+                                    <li><a href="#">Contribua</a></li>
+                                    <li><a href="#">API</a></li>
+                                    <li><a href="#">Suporte</a></li>
+                                </ul>
+                                <div className="left">
+                                    <ul className="primary">
+                                        <li className="logo">
+                                            <a href="/">
+                                                <img src="https://www.themoviedb.org/static_cache/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" title="The MovieDB" alt="The MovieDB" width="91" height="81" />
+                                                <span className="title">The Movie DB</span>
+                                                {/* <Glyphicon glyph="asterisk" /> */}
+                                            </a>
+                                        </li>
+                                        <li><a href="#">Explorar</a></li>
+                                        <li><a href="#">Filmes</a></li>
+                                        <li><a href="#">Séries</a></li>
+                                        <li><a href="#">Pessoas</a></li>
+                                    </ul>
+                                </div>
+                                <div className="right">
+                                    <ul className="primary">
+                                        <li><a href="#"><Glyphicon glyph="plus" /></a></li>
+                                        <li><a href="#"><Glyphicon glyph="globe" /></a></li>
+                                        <li><a href="#"><Glyphicon glyph="bell" /></a></li>
+                                        <li className="user">
+                                            <a href="#">
+                                                <div className="avatar">
+                                                    <img src="https://via.placeholder.com/32x32?text=U" alt="User" />
+                                                </div>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
