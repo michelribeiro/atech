019a1b1a51677006d15289511ef8aac08056b3dd
Michel Ribeiro <michelribeiro2004@gmail.com>
2018-04-25 15:32:22 -0300

    subindo para gh pages

--- src/config/path.jsx
+++
@@ -1,9 +0,0 @@
-'use strict'
-export const paths = {
-  PATH_BASE    :"http://api.themoviedb.org/3/",
-  PATH_DISCOVER   :"discover/movie",
-  MOVIE      :"movie/",
-  KEY        :"?api_key=2b9dda68a6100edd23850576cdb50d06",
-  PATH_IMAGE :"https://image.tmdb.org/t/p/w500/",
-  GENRE: "https://api.themoviedb.org/3/genre/movie/list"
-}
